901420b1d56a539df4a133c02c834fcee7643dca
AA-ishi <ishidasako@yahoo.co.jp>
2025-12-30 21:00:48 +0900

    Update main_ui.py

--- main_ui.py
+++ main_ui.py
@@ -118,7 +118,7 @@ st.markdown("""
 
 col1, col2, col3 = st.columns([1, 1, 1])
 with col2:
-    action = st.button("クリックしてね 💧🌿", key="main_button")
+    clicked = st.button("クリックしてね 💧🌿", key="main_button")
 
 # ===============================
 # 水やり頻度計算
